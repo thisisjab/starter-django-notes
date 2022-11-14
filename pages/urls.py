1dcca4b6c2a584a36c8c614629deaeaef9677793
thisisjab <heythisisjab@gmail.com>
2022-11-14 13:37:51 +0330

    Fix typo error in pages/urls

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home_page_view
 
 
-url_patterns = [
+urlpatterns = [
     # `name` is optional
     # if user requests home page represented in empty string, it will use `home_page_view`
     path('', home_page_view, name='home'),
