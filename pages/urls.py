f0eaab15f363bd323677105288254eb6039bbb8f
thisisjab <heythisisjab@gmail.com>
2022-11-15 13:01:28 +0330

    Create a TemplateView and modify pages/urls

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, HomePageView
 
 
 urlpatterns = [
     # `name` is optional
     # if user requests home page represented in empty string, it will use `home_page_view`
-    path('', home_page_view, name='home'),
+    path('', HomePageView.as_view(), name='home'),
 ]
