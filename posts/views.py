eb50a228d3a1b92eda007adc405911f4a1c7f133
thisisjab <heythisisjab@gmail.com>
2022-11-18 15:27:40 +0330

    Add posts page to the project
    - django_project/urls.py include posts.urls
    - create a list view in posts/views
    - create posts.html
    - create urls file for posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostsView(ListView):
+    model = Post
+    template_name = "posts.html"
