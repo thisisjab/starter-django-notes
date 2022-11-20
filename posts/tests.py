aa2c6d5a4c6642956ae2d00ae2d9403bbf76f847
thisisjab <heythisisjab@gmail.com>
2022-11-20 23:40:00 +0330

    Add more tests to post model

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from .models import Post
 
 
@@ -9,3 +10,13 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test.")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_posts_page(self):
+        response = self.client.get(reverse("posts"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "posts.html")
+        self.assertContains(response, "This is a test.")
