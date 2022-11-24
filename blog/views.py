9b240196d0644cafc84d014f9aa97998082a3f3c
thisisjab <heythisisjab@gmail.com>
2022-11-24 11:36:12 +0330

    Add blog home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class BlogHomeView(ListView):
+    model = Post
+    template_name = "bloghome.html"
