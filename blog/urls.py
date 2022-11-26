50a8a0b0205aeb24e5436b4e7f6b553ad5e4b5d5
thisisjab <heythisisjab@gmail.com>
2022-11-26 13:04:24 +0330

    Add individual blog posts page
    - Update urls
    - Create blogpost.html
    - Add a DetailView for the model Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BlogHomeView
+from .views import BlogHomeView, BlogPostDetailView
 
 
 urlpatterns = [
+    path("post/<int:pk>/", BlogPostDetailView.as_view(), name="blog_post_detail"),
     path("", BlogHomeView.as_view(), name="bloghome")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class BlogHomeView(ListView):
     model = Post
     template_name = "bloghome.html"
+
+
+class BlogPostDetailView(DetailView):
+    model = Post
+    template_name = "blogpost.html"
+    # context_object_name = "post" or "object"
+    # You can use `context_object_name` to specify a custom name for this context
