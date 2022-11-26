540f5e79240b6b1f824b8deee62a583047e7db44
thisisjab <heythisisjab@gmail.com>
2022-11-26 13:40:19 +0330

    Add create new post view to the blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BlogHomeView, BlogPostDetailView
+from .views import BlogHomeView, BlogCreatePostView, BlogPostDetailView
 
 
 urlpatterns = [
+    path("post/new/", BlogCreatePostView.as_view(), name="blog_new_post"),
     path("post/<int:pk>/", BlogPostDetailView.as_view(), name="blog_post_detail"),
     path("", BlogHomeView.as_view(), name="bloghome")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -12,3 +13,9 @@ class BlogPostDetailView(DetailView):
     template_name = "blogpost.html"
     # context_object_name = "post" or "object"
     # You can use `context_object_name` to specify a custom name for this context
+
+
+class BlogCreatePostView(CreateView):
+    model = Post
+    template_name = "blogcreate.html"
+    fields = ["title", "author", "body"]
