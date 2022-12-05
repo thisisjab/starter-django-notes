bbde585c025b98eaf74862e2a90eab919f93971a
thisisjab <heythisisjab@gmail.com>
2022-12-05 22:05:06 +0330

    Add accounts app + sign up functionality

--- django_project/urls.py
+++ django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include("posts.urls")),
     path('blog/', include("blog.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
